5fc4d7fcc41a30c6422be16bccb25f1aa03cb8e5
sargy <sargy.lukina@mytona.com>
2021-02-10 21:09:44 +0300

    game init

--- 004-stream/coin.js
+++ 004-stream/coin.js
@@ -1,18 +1,36 @@
 #!/usr/bin/env node
 const readline = require("readline");
-const fs = require('fs');
+const fs = require("fs");
+const path = require("path");
+
 const maxNumber = 1;
 const number = 1 + Math.round(Math.random() * maxNumber);
 
-const input = readline.createInterface(process.stdin);
-console.log(`Монета подкинута, орел - 1, решка - 2, угадайте число`);
-
-input.on("line", (data) => {
-    if (Number(data) === number) {
-        console.log(`Вы угадали`);
-        input.close();
-    } else {
-        console.log(`Вы не угадали`);
-        input.close();
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+console.log("Введите название файла");
+let fileName = "defaultLog.txt";
+rl.on("line", (data) => {
+    fileName = data.toString();
+    if (!!fileName) {
+        const file = path.join(__dirname, fileName);
+        rl.question("угадайте число 1 или 2:", (answer) => {
+            if (Number(answer) === number) {
+                console.log(`Вы угадали!`);
+                logResult("win", file);
+                rl.close();
+            } else {
+                console.log(`Вы не угадали :(`);
+                logResult("lose", file);
+                rl.close();
+            }
+        });
     }
 });
+const logResult = (result, file) => {
+    fs.appendFile(file, `${result}\n`, (err) => {
+        if (err) throw new Error(err);
+    });
+};

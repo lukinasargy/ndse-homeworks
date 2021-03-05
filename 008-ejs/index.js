18cf2f0527fc0324b89927048d5381398ad87847
sargy <sargy.lukina@mytona.com>
2021-03-05 21:05:12 +0300

    init

--- 008-ejs/index.js
+++ 008-ejs/index.js
@@ -6,19 +6,23 @@ const loggerMiddleware = require('./middleware/logger');
 const errorMiddleware = require('./middleware/error');
 
 const indexRouter = require('./routes/index');
-const booksRouter = require('./routes/books');
+const booksApiRouter = require('./routes/api/books');
 const userRouter = require('./routes/user');
+const booksRouter = require('./routes/books')
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.set("view engine", "ejs");
 app.use(cors());
 app.use(loggerMiddleware);
 
 app.use('/public', express.static(__dirname+"/public"));
 
 app.use('/', indexRouter);
-app.use('/api/books', booksRouter);
+app.use('/books', booksRouter);
+app.use('/api/books', booksApiRouter);
 app.use('/api/user', userRouter);
 
 app.use(errorMiddleware);
--- 008-ejs/routes/api/books.js
+++ 008-ejs/routes/api/books.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { Book } = require("../models");
-const fileMiddleware = require("../middleware/file");
+const { Book } = require("../../models");
+const fileMiddleware = require("../../middleware/file");
 
 const stor = {
     books: [],
